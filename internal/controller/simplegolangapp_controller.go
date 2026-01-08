854c03431b572fd8f62d99042461ce2e3a638f7d
wenqiang li <zooeymoon1989@gmail.com>
2026-01-08 23:16:33 +0800

    add event rabc

--- internal/controller/simplegolangapp_controller.go
+++ internal/controller/simplegolangapp_controller.go
@@ -98,7 +98,7 @@ func ptr[T any](v T) *T { return &v }
 // 如果你未来需要读 Gateway（校验 parentRefs、或者自动发现 listener）
 // +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways,verbs=get;list;watch
 //	添加事件event功能的权限
-// +kubebuilder:rbac:groups=events.k8s.io,resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=events.k8s.io,resources=events,verbs=create;patch;update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
